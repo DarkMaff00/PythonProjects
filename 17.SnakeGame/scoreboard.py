64b2fa264dd1e7fb0a50ceebd74ea906e09c6f54
Konrad Wojtowicz <konrad.wojtowicz@interia.pl>
2023-03-25 15:33:17 +0100

    Upgrade for Snake Game

--- 17.SnakeGame/scoreboard.py
+++ 17.SnakeGame/scoreboard.py
@@ -8,6 +8,8 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -15,13 +17,21 @@ class ScoreBoard(Turtle):
         self.print_score()
 
     def print_score(self):
-        self.write(f"Score:  {self.score}", align="center", font=FONT)
+        self.clear()
+        self.write(f"Score:  {self.score} \tHigh Score: {self.high_score}", align="center", font=FONT)
 
-    def game_over(self):
-        self.sety(0)
-        self.write("GAME OVER", align="center", font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.high_score))
+        self.score = 0
+        self.print_score()
+
+    # def game_over(self):
+    #     self.sety(0)
+    #     self.write("GAME OVER", align="center", font=FONT)
 
     def update(self):
         self.score += 1
-        self.clear()
         self.print_score()
