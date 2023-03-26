42ccce6728a832c660b2a6d8886919fde630b46a
Konrad Wojtowicz <konrad.wojtowicz@interia.pl>
2023-03-26 12:21:55 +0200

    Upgrade for Snake Game

---
+++ 20.MailMerge/main.py
@@ -0,0 +1,12 @@
+
+names = []
+with open("./Input/Names/invited_names.txt") as data:
+    lines = data.readlines()
+    for line in lines:
+        names.append(line.strip())
+
+with open("./Input/Letters/starting_letter.txt") as template:
+    letter_template = template.read()
+    for name in names:
+        with open(f"./Output/ReadyToSend/letter_for_{name}.txt", mode="w") as new_letter:
+            new_letter.write(letter_template.replace("[name]", name))
